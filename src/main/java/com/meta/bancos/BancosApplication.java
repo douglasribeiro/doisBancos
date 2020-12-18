53695ef1063ad3b42c25ae3020e24364923c86dc
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-12-18 10:25:00 -0300

    inicial

---
+++ src/main/java/com/meta/bancos/BancosApplication.java
@@ -0,0 +1,13 @@
+package com.meta.bancos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosApplication.class, args);
+	}
+
+}
